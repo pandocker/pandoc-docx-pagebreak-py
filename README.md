efc0cc4c21d1e2db3254cd2dd4f5f0294ea25688
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2018-05-10 13:51:50 +0900

    set up for pip installation
    - local test(-f markdown -t docx) worked

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # pandoc-docx-pagebreak-py
+
 Pandoc filter to insert pagebreak in docx file
+
+### install
+
+```sh
+pip3 install git+https://github.com/pandocker/pandoc-docx-pagebreak-py
+```
+
+### Usage
+
+Add `\newpage` where preferred to insert a page break
+
+Expecting to work like native pandoc behavior for latex output
+
+```sh
+pandoc -f markdown -t docx -o docx.docx --filter=pandoc-docx-pagebreakpy
+```
+
+### Sample
+
+```markdown
+Contents before pagebreak
+
+\\newpage
+
+Contents after pagebreak
+```
---
+++ docx_pagebreak/__init__.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" pandoc-docx-pagebreakpy
+Pandoc filter to insert pagebreak as openxml RawBlock
+Only for docx output
+
+Trying to port pandoc-doc-pagebreak
+- https://github.com/alexstoick/pandoc-docx-pagebreak
+"""
+
+import panflute as pf
+
+
+class DocxPagebreak(object):
+    pagebreak = pf.RawBlock("<w:p><w:r><w:br w:type=\"page\" /></w:r></w:p>", format="openxml")
+
+    def action(self, elem, doc):
+        if (doc.format == "docx"):
+            if isinstance(elem, pf.Para):
+                sub = elem.content[0]
+                if isinstance(sub, pf.Str) and sub.text == r"\newpage":
+                    pf.debug("Page Break")
+                    elem = self.pagebreak
+        return elem
+
+
+def main(doc=None):
+    dp = DocxPagebreak()
+    return pf.run_filter(dp.action, doc=doc)
+
+
+if __name__ == "__main__":
+    main()
