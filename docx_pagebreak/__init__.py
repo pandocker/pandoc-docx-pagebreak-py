33fe30cc86bd46c9b017999e5e5b56584ce6b7c0
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2018-05-18 18:25:45 +0900

    add TOC command(`\toc`)

--- docx_pagebreak/__init__.py
+++ docx_pagebreak/__init__.py
@@ -16,7 +16,33 @@ class DocxPagebreak(object):
     pagebreak = pf.RawBlock("<w:p><w:r><w:br w:type=\"page\" /></w:r></w:p>", format="openxml")
     sectionbreak = pf.RawBlock("<w:p><w:pPr><w:sectPr><w:type w:val=\"nextPage\" /></w:sectPr></w:pPr></w:p>",
                                format="openxml")
-
+    toc = pf.RawBlock("""
+<w:sdt>
+<w:sdtPr>
+  <w:docPartObj>
+    <w:docPartGallery w:val="Table of Contents" />
+  </w:docPartObj>
+</w:sdtPr>
+<w:sdtContent xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main">
+  <w:p>
+    <w:pPr>
+      <w:pStyle w:val="TOCHeading" />
+    </w:pPr>
+    <w:r>
+      <w:t xml:space="preserve">Table of Contents</w:t>
+    </w:r>
+  </w:p>
+  <w:p>
+    <w:r>
+      <w:fldChar w:fldCharType="begin" w:dirty="true" />
+      <w:instrText xml:space="preserve">TOC \o "1-3" \h \z \u</w:instrText>
+      <w:fldChar w:fldCharType="separate" />
+      <w:fldChar w:fldCharType="end" />
+    </w:r>
+  </w:p>
+</w:sdtContent>
+</w:sdt>
+""")
     def action(self, elem, doc):
         if isinstance(elem, pf.RawBlock):
             if elem.text == r"\newpage":
@@ -27,6 +53,12 @@ class DocxPagebreak(object):
                 if (doc.format == "docx"):
                     pf.debug("Section Break")
                     elem = self.sectionbreak
+                else:
+                    elem = []
+            elif elem.text == r"\toc":
+                if (doc.format == "docx"):
+                    pf.debug("Table of Contents")
+                    elem = self.toc
                 else:
                     elem = []
         return elem
