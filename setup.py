58f3f172bb4491caaae60f40985a1016042745a1
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2018-11-25 15:07:41 +0900

    let setuptools decide version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 # Arguments marked as "Required" below must be included for upload to PyPI.
 # Fields marked as "Optional" may be commented out.
 
-VERSION = "0.0.1"
+# VERSION = "0.0.1"
 REQUIRES = ["panflute"]
 setup(
     # This is the name of your project. The first time you publish this
@@ -34,7 +34,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name="pandoc-docx-pagebreakpy",  # Required
+    name="pandoc-docx-pagebreak",  # Required
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
@@ -42,12 +42,14 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version=VERSION,  # Required
+    # version=VERSION,  # Required
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description="Pandoc filter to insert pagebreak in docx file",  # Required
+    description="Pandoc filter for docx output to insert pagebreak at will",  # Required
 
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.
@@ -79,7 +81,7 @@ setup(
 
     # This should be your name or the name of the organization which owns the
     # project.
-    author="K4zuki/pandocker",  # Optional
+    author="Kazuki Yamamoto, pandocker",  # Optional
 
     # This should be a valid email address corresponding to the author listed
     # above.
