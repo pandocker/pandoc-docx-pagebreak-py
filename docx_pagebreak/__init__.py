b38b32ad20e463ad11301fbd1bce2a9fb25a4723
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2019-02-01 22:48:01 +0900

    [WIP] change ToC table

--- docx_pagebreak/__init__.py
+++ docx_pagebreak/__init__.py
@@ -17,31 +17,24 @@ class DocxPagebreak(object):
     sectionbreak = pf.RawBlock("<w:p><w:pPr><w:sectPr><w:type w:val=\"nextPage\" /></w:sectPr></w:pPr></w:p>",
                                format="openxml")
     toc = pf.RawBlock(r"""
-<w:sdt>
-<w:sdtPr>
-  <w:docPartObj>
-    <w:docPartGallery w:val="Table of Contents" />
-  </w:docPartObj>
-</w:sdtPr>
-<w:sdtContent xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main">
-  <w:p>
-    <w:pPr>
-      <w:pStyle w:val="TOC Heading" />
-    </w:pPr>
-    <w:r>
-      <w:t xml:space="preserve">Table of Contents</w:t>
-    </w:r>
-  </w:p>
+<w:sdtContent>
+    <w:p>
+        <w:pPr>
+            <w:pStyle w:val="TOC Heading" />
+        </w:pPr>
+        <w:r>
+            <w:t>Table of Contents</w:t>
+        </w:r>
+    </w:p>
   <w:p>
     <w:r>
-      <w:fldChar w:fldCharType="begin" w:dirty="true" />
+      <w:fldChar w:fldCharType="begin"/>
       <w:instrText xml:space="preserve">TOC \o "1-3" \h \z \u</w:instrText>
       <w:fldChar w:fldCharType="separate" />
       <w:fldChar w:fldCharType="end" />
     </w:r>
   </w:p>
 </w:sdtContent>
-</w:sdt>
 """, format="openxml")
 
     def action(self, elem, doc):
