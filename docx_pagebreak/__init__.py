40fc95c95f5ae7d745c319dc4b7c76c71bc5ab08
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2019-02-01 23:43:07 +0900

    dirty=true

--- docx_pagebreak/__init__.py
+++ docx_pagebreak/__init__.py
@@ -47,7 +47,7 @@ class DocxPagebreak(object):
         </w:p>
       <w:p>
         <w:r>
-          <w:fldChar w:fldCharType="begin"/>
+          <w:fldChar w:fldCharType="begin" w:dirty="true" />
           <w:instrText xml:space="preserve">TOC \o "1-3" \h \z \u</w:instrText>
           <w:fldChar w:fldCharType="separate" />
           <w:fldChar w:fldCharType="end" />
